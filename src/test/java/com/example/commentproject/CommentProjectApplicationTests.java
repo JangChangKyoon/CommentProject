ba37dfb9ad146aea83d0262e2191f9b9c5b05fcb
jack <jachky1205@gmail.com>
2022-08-05 11:20:03 +0900

    FirstSetting

---
+++ src/test/java/com/example/commentproject/CommentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
